74fdd57ca40d6fc175c0c9fc078cf9aa53755d39
ben <ben@saturne.(none)>
2016-11-06 02:07:30 +0100

    Clean up and add comments.

--- python/xslcover/htdocs/covertable.js
+++ python/xslcover/htdocs/covertable.js
@@ -1,5 +1,15 @@
-
-function myFunction(popup) {
+/*
+ * A popup is as follow:
+ *
+ * <span class="popup">
+ *   ... data to click on to show the popup ...
+ *   <span class="popuptext">Content of the popup</span>
+ * </span>
+ *
+ * The function just makes the popup visible, and hide the other popups
+ *
+ */
+function show_popup(popup) {
   var popuptext = popup.getElementsByClassName("popuptext")[0];
   popuptext.style.visibility = "visible";
   popuptext.style.animation = "fadeIn 1s";
@@ -17,10 +27,17 @@ function myFunction(popup) {
   }
 }
 
-//var toto = document.getElementsByTagName("body");
-
-//document.getElementsByTagName("body")[0].onload =
-
+/*
+ * The HTML stores a list of 'popuptext' class elements outside
+ * the table data. The principle is to clone all of the 'popuptext's and insert
+ * them into the 'covered' lines one by one, and add to the lines the 'popup'
+ * class, so that clicking on the line will show up the popup content.
+ *
+ * Once done, the document has elements like this:
+ *
+ * <span class="covered popup"> ... <span class="popuptext">...</span></span>
+ *
+ */
 function fill_popups() {
   var popuptexts = document.getElementsByClassName("popuptext");
   var popups_to_fill =  document.getElementsByClassName("covered")
@@ -35,7 +52,7 @@ function fill_popups() {
     popups_to_fill[i].classList.add("popup")
     popups_to_fill[i].onclick = function(e){
       var k = this;
-      myFunction(k);
+      show_popup(k);
     }
   }
 }
